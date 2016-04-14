6a83ef1657b07125c58b1b2b203c8eb60e95a266
Steve Zhu <st.zhu1@gmail.com>
2016-04-13 22:50:48 -0500

    Remove unecessary credentials object and log

--- dars.js
+++ dars.js
@@ -1,12 +1,6 @@
 const cp = require('child_process');
 const argv = require('yargs').argv;
 
-var credentials = {
-  username: argv.username,
-  password: argv.password
-};
-console.log('argv', JSON.stringify(credentials));
-
 const casper_process = cp.spawn('casperjs', [
   'casper_dars.js',
   '--username=' + argv.username,
